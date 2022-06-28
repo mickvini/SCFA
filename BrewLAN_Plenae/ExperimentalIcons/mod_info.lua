c3b15f50c0639c34988f685b43ab42e4d752b633
mickvini <93882097+mickvini@users.noreply.github.com>
2022-06-28 06:37:21 +0300

    BrewLAN_Plenae

---
+++ BrewLAN_Plenae/ExperimentalIcons/mod_info.lua
@@ -0,0 +1,14 @@
+name = "Experimental Icons Overhaul"
+uid = "2529ea71-93ef-41a6-b552-EXPERICON00009"
+version = 0.9
+copyright = "© 2015–2018 Sean Wheeldon"
+description = "If you can't see the mod icon then it can't detect the SCD file, and probably wont work. Your installation is likely wrong."
+author = "Balthazar"
+icon = "/mods/BrewLAN_Plenae/ExperimentalIcons/Experimental-Icon-Overhaul_Mod_Icon.dds"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+conflicts = {  }
+before = {  }
+after = {  }
---
+++ BrewLAN_Plenae/Spomeniki/hook/loc/CZ/strings_db.lua
@@ -0,0 +1,18 @@
+--ISO_639-1_code=CS
+--Translator:  Oaks
+ssb23sn_desc="Těžká Obranná Věž"
+ssb23sn_name="Ytho"
+Unit_Description__Spomeniki_001="Obranná věž. Vystřeluje pomalé mocné energetické výboje se středním doletem. Uděluje masivní poškození"
+
+
+
+
+
+
+
+
+
+
+
+
+
