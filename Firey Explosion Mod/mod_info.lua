93524f6e2fa8c01511295857b0327ab12f17c7ac
mickvini <93882097+mickvini@users.noreply.github.com>
2022-06-28 06:45:16 +0300

    Firey Explosion Mod

---
+++ Firey Explosion Mod/mod_info.lua
@@ -0,0 +1,13 @@
+name = "Firey Explosions mod 2.6 Beta"
+version = 2.6
+copyright = "Copyright © 2013, Luxor"
+author = "Me"
+description = "Mod features improved destruction and environmental effects and few new mechanics to make blasting those bastards even more satisfying."
+url = "none"
+
+uid = "dda54b6c-bd29-4e08-8a88-a8db22493dcb"
+exclusive = false
+ui_only = false
+selectable = true
+icon = "/mods/Firey Explosion Mod/Icon.jpg" 
+before = { }
