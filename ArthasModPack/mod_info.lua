96250cdb76549356b78dab12c0634548f8d7040c
mickvini <93882097+mickvini@users.noreply.github.com>
2022-06-28 07:23:56 +0300

    ArthasModPack

---
+++ ArthasModPack/mod_info.lua
@@ -0,0 +1,20 @@
+name = "Imperator Arthas ModPack v0.1"
+version = 0.1
+author = "Arthas"
+description = "My ModPack for game"
+
+uid = "61ed5195-c7bb-4813-b4a5-6ae8a5fe74ec"
+
+url = "https://vk.com/club92069860"
+icon = "/mods/ArthasModPack/icon.png"
+
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { "9e8ea941-c306-4751-b367-f00000000005" }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { }
+ 
